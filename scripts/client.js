08eddc3269e1378cfcd65ed23b4b288b5466d6cd
Connor Peck <connorjamespeck@gmail.com>
2022-04-16 13:22:35 -0500

    monthly cost on the dom

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,7 @@ $(document).ready( onReady);
 function onReady(){
     $('#submitInfoButtonIn').on('click', employeeInfoIn)
     $('#submitInfoButtonIn').on('click', displayInfo)
+    $('#submitInfoButtonIn').on('click', clearInputs)
 }
 employeeInfo =[];
 
@@ -19,21 +20,30 @@ function employeeInfoIn(){
 
 
 } // end employeeInfoIn
-
+let monthlyCost = 0
 function displayInfo(){
     console.log('in display info ');
    let el = $('#infoOut');
     el.empty();
     for (let i=0; i<employeeInfo.length; i++){
     el.append(`<li> ${employeeInfo[i].firstName}, ${employeeInfo[i].lastName},  ${employeeInfo[i].ID},${employeeInfo[i].jobTitle} : ${employeeInfo[i].annualSalary} </li>`)
+    
+    monthlyCost += Number(employeeInfo[i].annualSalary / 12);
+    console.log(monthlyCost);
+    $('#monthlyCostOut').empty().append('<h4>' +' Monthly Cost: ' + '$' +  monthlyCost  + '</h4>');
+    if (monthlyCost >= 20000){
+        console.log('too much');
     }
 }
+    
+}
    // clearing inputs
+   function clearInputs(){
    $('#firstNameIn').val('')
    $('#lastNameIn').val('')
    $('#idIn').val('')
    $('#jobTitleIn').val('')
-   $('#annualSalaryIn').val('')
+   $('#annualSalaryIn').val('')}
 
   
 
